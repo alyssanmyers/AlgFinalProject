e66e89122c1534f2418ca1e7e31b7619703e2be1
Luciano Mogorovic <chon@Lucianos-MacBook-Pro.local>
2017-04-27 10:37:30 -0400

    Initial code

---
+++ SeqAlign.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+
+// set up matrix
+char** setUpMatrix(std::string s1, std::string s2);
+
+
+int main(int argc, char* argv[]){
+    
+
+
+}
