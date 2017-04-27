49fc5e41eea8bfdab912afa677d655f7490a0251
Luciano Mogorovic <chon@Lucianos-MacBook-Pro.local>
2017-04-27 11:13:59 -0400

    set up first row and column with gap penalty

--- SeqAlign.cpp
+++ SeqAlign.cpp
@@ -12,8 +12,29 @@ int main(int argc, char* argv[]){
 
     std::string seq2 = argv[2];
 
-    int seq1L = seq1.length();
-    int seq2L = seq2.length();
+    int seq1L = seq1.length() + 1;
+    int seq2L = seq2.length() + 1;
+
+    char Matrix[seq1L][seq2L];
+
+    int gap = -2;
+    int match = 1;
+    int nmatch = -1;
+
+    // set up first row with gap value
+    for(int i = 0; i < seq2L; ++i){
+
+        Matrix[0][i] = i * gap;
+
+    }
+
+    // set up first column with gap value
+    for(int i = 0; i < seq1L; ++i){
+
+        Matrix[i][0] = i * gap;
+        
+    }
+
 
 
 }
